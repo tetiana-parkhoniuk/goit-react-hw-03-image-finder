8b9e614abe0ad6f8fdac454c0c6bb8a8f757456c
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-07-20 23:17:07 +0300

    Adds App and Searchbar.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import './App.css';
+import { Component } from 'react';
+import Searchbar from 'components/Searchbar';
+import styles from './App.module.css';
 
-function App() {
-  return <div className="App">test</div>;
+export default class App extends Component {
+  state = {
+    searchQuery: null,
+    images: [],
+    reqStatus: '',
+    // idle, pending, fulfilled, rejected
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+    console.log(searchQuery);
+  };
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,27 @@
+import styles from './Searchbar.module.css';
+
+export default function Searchbar({ onSubmit }) {
+  const handleSearch = event => {
+    event.preventDefault();
+    onSubmit(event.target.elements.searchQuery.value);
+  };
+
+  return (
+    <header className={styles.searchbar}>
+      <form className={styles.searchForm} onSubmit={handleSearch}>
+        <button type="submit" className={styles.searchFormBtn}>
+          <span className={styles.searchFormBtnLabel}>Search</span>
+        </button>
+
+        <input
+          className={styles.searchFormInput}
+          name="searchQuery"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+}
