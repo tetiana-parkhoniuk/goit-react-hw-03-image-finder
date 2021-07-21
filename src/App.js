c9c9aedcd65012cf9cad68ca3380d69443cb6c83
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-07-21 18:58:27 +0300

    Adds Loader but still have some errors in console.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery ';
 import Button from 'components/Button';
+import Spinner from 'components/Spinner';
 import { fetchImages } from 'services/api';
 import styles from './App.module.css';
 
@@ -16,24 +17,42 @@ export default class App extends Component {
 
   async componentDidUpdate(_, prevState) {
     if (prevState.searchQuery !== this.state.searchQuery) {
-      this.setState({ images: [] });
-      this.resetPage();
-
       try {
+        this.setState({ images: [], reqStatus: 'pending' });
+        this.resetPage();
+
         const images = await fetchImages(
           this.state.searchQuery,
           this.state.page,
         );
-        this.setState({ images });
+        this.setState({ images, reqStatus: 'fulfilled' });
       } catch (error) {
+        this.setState({ reqStatus: 'rejected' });
         alert('Error');
       }
     }
 
     if (prevState.page !== this.state.page) {
-      const images = await fetchImages(this.state.searchQuery, this.state.page);
-      this.setState({ images: [...this.state.images, ...images] });
-      this.scrollOnLoadMore();
+      try {
+        this.setState({ reqStatus: 'pending' });
+
+        const images = await fetchImages(
+          this.state.searchQuery,
+          this.state.page,
+        );
+        this.setState({
+          images: [...this.state.images, ...images],
+          reqStatus: 'fulfilled',
+        });
+        this.scrollOnLoadMore();
+      } catch (error) {
+        this.setState({ reqStatus: 'rejected' });
+        alert('Error');
+      }
+
+      // const images = await fetchImages(this.state.searchQuery, this.state.page);
+      // this.setState({ images: [...this.state.images, ...images] });
+      // this.scrollOnLoadMore();
     }
   }
 
@@ -57,13 +76,17 @@ export default class App extends Component {
   };
 
   render() {
-    const { images } = this.state;
+    const { images, reqStatus } = this.state;
+    const isLoading = reqStatus === 'pending';
     const showLoadMoreBtn = images.length > 0;
+
     return (
       <div className={styles.app}>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery images={this.state.images} />
+        {isLoading && <Spinner />}
+        {<ImageGallery images={this.state.images} />}
         {showLoadMoreBtn && <Button onClick={this.incrementPage} />}
+        {/* { isLoading && !showLoadMoreBtn && <Spinner/>} */}
       </div>
     );
   }
